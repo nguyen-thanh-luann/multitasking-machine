6a645dc8d83bf54ab18c6bdf23b688edee7afb78
thanhluann <imthanhluan@gmail.com>
2022-06-12 23:32:13 +0700

    first update

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+
+function calcFahrenheit(){
+    let cel = document.querySelector('#celsius').value;
+    document.querySelector('#fahrenheit').value =  celToFah(cel);
+}
+
+function calcCelsius(){
+    let fah = document.querySelector('#fahrenheit').value;
+    document.querySelector('#celsius').value = fahToCel(fah);
+}
+
+function fahToCel(fah){
+    return Number.parseFloat((fah - 32) * (5/9)).toFixed(1);
+}
+
+function celToFah(cel){
+    return Number.parseFloat((cel / (5/9)) + 32).toFixed(1);
+}
